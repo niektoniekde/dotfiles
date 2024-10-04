96a6fefe3ea69eddacf414d8d4ee59dac98f5c15
Jaroslav Petráš <niekto@niekde.sk>
2024-10-04 17:27:00 +0200

    add shortcuts

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -11,6 +11,8 @@ cnoremap <expr> <Right> pumvisible() ? "\<Down>"  : "\<Right>"
 
 " delete all buffers except current one
 nnoremap <Leader>xo :1,.-bd\|.+,$bd<CR>
+nnoremap <Leader>xb :1,.-bd<CR>
+nnoremap <Leader>xa :.+,$bd<CR>
 " delete current buffer
 nnoremap <Leader>xc :bd<CR>
 ]]
